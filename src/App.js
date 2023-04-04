49ad445dddbcdc9c987dc793afd6dff02bfab025
saransh2singh <110494720+saransh2singh@users.noreply.github.com>
2023-04-04 21:12:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Components/Header';
+import Meme from './Components/Meme';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Meme />
+    </div>
+  );
+}
+
+export default App;
